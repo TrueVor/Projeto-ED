ccce34ef9a8221183bf5246cca793a9c1045abf5
jvmoraisreis <58453291+jvmoraisreis@users.noreply.github.com>
2020-07-18 18:04:25 -0300

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -15,24 +15,39 @@ SeqSet::SeqSet() {
     arq.close();
 }
 
-Bloco SeqSet::BuscarBloco(unsigned _t) {
+Bloco SeqSet::BuscarBloco(pacote& _p) {
     Bloco aux;
     bool achou = false;
     ifstream arq;
-    if (arq){
+    if (arq) {
         arq.read((char*) &cabSS, sizeof(Cabecalho));
         if (cabSS.num == 0) {
              throw runtime_error("Erro na busca: arquivo vazio!");
         }
+        //nesse ponto o ponteiro de leitura já vai estar no final do cabeçalho 
         else {
-            while (achou == false && aux.cabBloco.proximo != -1) {
-                arq.seekg(sizeof(Bloco)*cabSS.posPrimeiro); //coloca o ponteiro de leitura no primeiro bloco
-                arq.read((char*) &aux, sizeof(Bloco)); //passa o bloco do arquivo pra memória
+            arq.read((char*) &aux, sizeof(Bloco)); //lê o primeiro Bloco
+            unsigned tam = aux.cabBloco.quantidade -1; //pos do ultimo elemento valido do vetor
+            if (_p.tamanho < aux.dados[tam].tamanho) { //se o elemento pertencer ao primeiro bloco
+                return aux; 
             }
+            else {
+                while ((achou == (false)) and (aux.cabBloco.proximo != -1)) {
+                    arq.seekg(sizeof(Bloco)*aux.cabBloco.proximo); //arruma o ponteiro de leitura
+                    arq.read((char*) &aux, sizeof(Bloco)); //passa o bloco do arquivo pra memória
+                    tam = aux.cabBloco.quantidade -1;
+                    if (_p.tamanho < aux.dados[tam].tamanho) {
+                        achou = true;
+                        return aux;
+                    }
+                }
+            }
+            aux.idBloco = -1; //manda um bloco inválido
             return aux;
         }
     }
 }
+
 
 void SeqSet::Inserir(pacote& _p) {
     Bloco aux;
